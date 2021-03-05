c8f6dd3f4ad281e84a93ef02d906dd5646bfe72c
Claudiavp01 <claudia.villar@estudiants.urv.cat>
2021-03-05 12:47:55 +0100

    second version

--- src/cat/urv/deim/asm/labs/Log.java
+++ src/cat/urv/deim/asm/labs/Log.java
@@ -1,4 +1,30 @@
 package cat.urv.deim.asm.labs;
 
-public class Log {
+import java.awt.*;
+import java.util.Date;
+public class Log extends  ConsoleLog {
+
+
+public  Log(){}
+
+    public void error(String tag, String message){
+        System.out.println("\033[31m" + fecha() + " (E) " + tag + ": " + message);
+    }
+
+    public void warning(String tag, String message){
+        System.out.println( "\033[33m" + fecha() + " (W) " + tag + ": " + message );
+    }
+
+    public void debug(String tag, String message){
+        System.out.println("\u001B[36m" + fecha() + " (D) " + tag + ": " + message);
+    }
+    public void info(String tag, String message){
+        System.out.println("\033[35m" + fecha() + " (I) " + tag + ": " + message);
+    }
+
+    public void verbose(String tag, String message){
+        System.out.println("\033[30m" + fecha() + " (V) " + tag + ": " + message);
+    }
+
+
 }
